9371919a57a9aae216f25d350ef05b8f8fe03d0e
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2023-07-17 22:04:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-Mulish">
+    <RouterView/>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
